b850431bcf216b634e3ebe1aa59aeac5535a9df3
Jordan Butler <20106583@mail.wit.ie>
2026-01-28 14:53:27 +0000

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -19,6 +19,12 @@ function App() {
       console.log(`${taskIndex} ${tasks[taskIndex].done}`);
     }
 
+    const deleteHandler = (taskIndex) => {
+      const tasks = [...taskState.tasks];
+      tasks.splice(taskIndex, 1);
+      setTaskState({tasks});
+  } 
+
     return (
     <div className="container">
       <h1>Tasky</h1>
@@ -31,6 +37,8 @@ function App() {
         key={task.id}
         done={task.done}
         markDone={() => doneHandler(index)}
+        deleteTask = {() => deleteHandler(index)}
+
       />
     ))} 
 
