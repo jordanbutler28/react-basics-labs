4c4011a72c87d65b5ba8e8ff93e795fece522bfc
Jordan Butler <20106583@mail.wit.ie>
2026-01-28 14:46:55 +0000

    Mark task as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -6,22 +6,31 @@ function App() {
 
     const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1,title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "High" },
-      { id: 2,title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority:"Medium"},
-      { id: 3,title: "Tidy up", description: "Put away toys and books", deadline: "Today", priority:"Low"}
-    ]
-  });
+      { id: 1,title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "High", done: false },
+      { id: 2,title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority:"Medium", done: false},
+      { id: 3,title: "Tidy up", description: "Put away toys and books", deadline: "Today", priority:"Low", done: false}
+      ]
+    });
+
+    const doneHandler = (taskIndex) => {
+      const tasks = [...taskState.tasks];
+      tasks[taskIndex].done = !tasks[taskIndex].done;
+      setTaskState({tasks});
+      console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+    }
 
     return (
     <div className="container">
       <h1>Tasky</h1>
-        {taskState.tasks.map((task) => (              
+        {taskState.tasks.map((task, index) => (              
       <Task 
         title={task.title}
         description={task.description}
         deadline={task.deadline}
         priority = {task.priority}
         key={task.id}
+        done={task.done}
+        markDone={() => doneHandler(index)}
       />
     ))} 
 
